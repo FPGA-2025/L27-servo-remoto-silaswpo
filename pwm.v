94aa2849098eba170e29d69b36ec7155ecd1328d
silaswpo <silaswesleypereiradeoliveira@gmail.com>
2025-06-09 22:17:37 -0300

    validando

--- pwm.v
+++ pwm.v
@@ -1,11 +1,25 @@
 module PWM (
     input wire clk,
     input wire rst_n,
-    input wire [31:0] duty_cycle, // duty cycle final = duty_cycle / period
-    input wire [31:0] period, // pwm_freq = clk_freq / period
+    input wire [31:0] duty_cycle,
+    input wire [31:0] period,
     output reg pwm_out
 );
 
-//insira o seu código aqui
-    
+    reg [31:0] counter;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            counter <= 0;
+            pwm_out <= 0;
+        end else begin
+            if (counter < period)
+                counter <= counter + 1;
+            else
+                counter <= 0;
+
+            pwm_out <= (counter < duty_cycle) ? 1'b1 : 1'b0;
+        end
+    end
+
 endmodule
--- servo.v
+++ servo.v
@@ -1,12 +1,40 @@
 module servo #(
-    parameter CLK_FREQ = 25_000_000, // 25 MHz
-    parameter PERIOD = 500_000 // 50 Hz (1/50s = 20ms, 25MHz / 50Hz = 500000 cycles)
-) (
+    parameter CLK_FREQ = 25_000_000,
+    parameter PERIOD = 40              // período de 40 ciclos
+)(
     input wire clk,
     input wire rst_n,
     output wire servo_out
 );
 
-//insira seu código aqui
+    // Pulso de 2 ciclos (5%) e 4 ciclos (10%)
+    localparam DUTY_MIN = 2;
+    localparam DUTY_MAX = 4;
+    localparam TIME_5S = CLK_FREQ * 5;
+
+    reg [31:0] current_duty = DUTY_MIN;
+    reg [31:0] counter = 0;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            counter <= 0;
+            current_duty <= DUTY_MIN;
+        end else begin
+            if (counter >= TIME_5S) begin
+                counter <= 0;
+                current_duty <= (current_duty == DUTY_MIN) ? DUTY_MAX : DUTY_MIN;
+            end else begin
+                counter <= counter + 1;
+            end
+        end
+    end
+
+    PWM pwm_inst (
+        .clk(clk),
+        .rst_n(rst_n),
+        .duty_cycle(current_duty),
+        .period(PERIOD),
+        .pwm_out(servo_out)
+    );
 
 endmodule
